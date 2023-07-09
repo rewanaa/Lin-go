1902f0815ac27da0f45108682dd235d5bb6ae444
MaiAteff <86915996+MaiAteff@users.noreply.github.com>
2023-07-09 03:13:18 +0300

    done

--- Lin-go/infere.sh
+++ Lin-go/infere.sh
@@ -4,6 +4,6 @@
 # The email of the leader same as the one on codalab leaderboard
 python3 infere.py --batch-size 1 \
                   --model-dir "/mnt/models/" \
-                  --leader-codalab-email "test@aic.gov.eg" \
+                  --leader-codalab-email "20201700275@cis.asu.edu.eg" \
                   --result-save-path "/mnt/submissions/" \
                   --data-path "/mnt/data/val.jsonl" \
